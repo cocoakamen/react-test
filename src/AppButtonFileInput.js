18130f018a747c9b251b2b119ef80f20f3dc378c
cocoakamen <coding.cocoa@gmail.com>
2021-05-23 20:37:33 +0900

    File API

--- src/AppButtonFileInput.js
+++ src/AppButtonFileInput.js
@@ -1,10 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function AppButtonFileInput(props) {
+  const [fileContent, setFileContent] = useState(null);
+
+  useEffect(() => {
+    console.log('file content',  fileContent);
+  })
+
+  const handleFile = (e) => {
+    const contentRows = e.target.result.split('\n');
+    setFileContent(contentRows);
+  }
+  
+  const handleChangeFile = (file) => {
+    let fileData = new FileReader();
+    fileData.onloadend = handleFile;
+    fileData.readAsText(file);
+  }
+
   return(
-    <label className="app-button-file-input">
-    ファイルを選択<input type="file" />
-    </label>
+    <div>
+      <label className="app-button-file-input">
+      ファイルを選択<input type="file" onChange={e => 
+            handleChangeFile(e.target.files[0])} />
+      </label>
+      <p>
+        {fileContent}
+      </p>
+    </div>
   );
 
 };
